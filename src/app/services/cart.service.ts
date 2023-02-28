3bd3a53a47c11a51c6a02599a92678ccfbbf9d71
Mohamed Kleiser Sarifo <kleiser.sarifo@gmail.com>
2023-02-28 19:55:31 +0200

    feat: Cart Service empty, remove and checkout

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,24 +1,31 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {OrderItem} from "../domain/OrderItem";
 import {Product} from "../domain/Product";
+import {Order} from "../domain/Order";
+import {Observable} from "rxjs";
+import {BASE_URL} from "../config/constants";
+import {HttpClient, HttpResponse} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  cartCount = new EventEmitter<number>();
+  constructor(private http: HttpClient) { }
 
   addItem(item: OrderItem) {
     let cartItems = this.getItems()
     if(!cartItems) {
       localStorage.setItem('my-cart', JSON.stringify([item]))
+      this.cartCount.emit(1)
     } else {
       let existing = cartItems.filter(cItem => cItem.product.id == item.product.id)
       if(existing && existing.length>0) {
         existing[0].quantity++
       } else {
         cartItems.push(item)
+        this.cartCount.emit(cartItems.length)
       }
       localStorage.setItem('my-cart', JSON.stringify(cartItems))
     }
@@ -39,14 +46,22 @@ export class CartService {
   }
 
   removeItem(product: Product): OrderItem[] {
-    let cartItems = localStorage.getItem('my-cart')
-    let items
-    if(cartItems && cartItems?.length > 0)
-      items = JSON.parse(cartItems as string)
-      items.splice(items.findIndex((element: { product: Product; }) => element.product === product),1);
-      localStorage.setItem('my-cart', JSON.stringify(items))
-      return items
+    let cartItems = this.getItems()
+    if(cartItems)
+      cartItems.splice(cartItems.findIndex((element:  OrderItem) => element.product.id === product.id),1);
+      localStorage.setItem('my-cart', JSON.stringify(cartItems))
+      this.cartCount.emit(cartItems.length)
+      return cartItems
     return []
+  }
 
+  emptyCart() {
+    localStorage.removeItem('my-cart')
+    this.cartCount.emit(0)
+  }
+
+  checkout(order: Order): Observable<HttpResponse<Order>> {
+    return this.http
+      .post<Order>(`${BASE_URL}/orders`, order, {observe: 'response'})
   }
 }
