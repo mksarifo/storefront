74cbbc4d2e26432a1e88fe87ceb62b4b04eb0480
Mohamed Kleiser Sarifo <kleiser.sarifo@gmail.com>
2023-03-02 06:56:14 +0200

    feat: Success alert after register

--- src/app/components/account/register/register.component.ts
+++ src/app/components/account/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators, AbstractControl} from '@angular/forms';
+import {AuthService} from "../../../services/auth.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,59 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private authService: AuthService) {
+  }
+  doNotMatch = false;
+  error = false;
+  errorEmailExists = false;
+  errorUserExists = false;
+  success = false;
+
+  // convenience getter for easy access to form fields
+  // https://stackoverflow.com/a/70134069/5152443
+  get form(): { [key: string]: AbstractControl; }
+  {
+    return this.registerForm.controls;
+  }
+
+  registerForm = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(5), Validators.maxLength(254), Validators.email],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(4), Validators.maxLength(50)],
+    }),
+    confirmPassword: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(4), Validators.maxLength(50)],
+    }),
+  });
+
+  register(): void {
+    this.doNotMatch = false;
+    this.error = false;
+    this.errorEmailExists = false;
+    this.errorUserExists = false;
+
+    const { password, confirmPassword } = this.registerForm.getRawValue();
+    if (password !== confirmPassword) {
+      this.doNotMatch = true;
+    } else {
+      const { email } = this.registerForm.getRawValue();
+      this.authService
+        .register({ email, password })
+        .subscribe({ next: () => (this.success = true), error: response => this.processError(response) });
+    }
+  }
+
+  private processError(response: HttpErrorResponse): void {
+    // TODO handle errors
+    if (response.status === 400) {
+      this.errorUserExists = true;
+    } else {
+      this.error = true;
+    }
+  }
 }
